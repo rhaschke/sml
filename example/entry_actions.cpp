d87b7c615e5dfc93b570636c844a902e5cf8e28d
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2023-02-18 16:28:56 +0100

    on_entry actions only allow for lambda expressions?
    Trying to bind free functions or member functions fails.
    Ideally, a state's on_entry member function would be considered.
    
    Only allowing free functions renders writing reusable code much harder.
    I was trying to implement a stateful state (having member `data`), which should
    be incremented on each entry of the state, for example.
    Using a global variable instead would be awful.

---
+++ example/entry_actions.cpp
@@ -0,0 +1,55 @@
+#include <boost/sml.hpp>
+#include <iostream>
+
+namespace sml = boost::sml;
+
+// events
+struct next {};
+
+// states
+struct s1 {};
+struct s2 {};
+
+/* clang-format on */
+
+auto print = []() { std::cout << "lambda triggered\n"; };
+
+void free_print() { std::cout << "free_print triggered\n"; }
+
+class Machine {
+  using Self = Machine;
+  int data{0};
+
+ public:
+  auto operator()() {
+    using namespace sml;
+    /* clang-format off */
+    return make_transition_table(
+      * "idle"_s      + on_entry<_> / print  // lambda expression works
+      , state<s1>     + on_entry<_> / &Self::static_print // static method fails
+      , state<s2>     + on_entry<_> / &free_print// free function fails
+      , "idle"_s      + event<next> = state<s1>
+		, state<s1>     + event<next> = state<s2>
+      , state<s2>     + event<next> = state<s1>
+    );
+    /* clang-format on */
+  }
+
+ private:
+  static void static_print() { std::cout << "static_print triggered\n"; };
+};
+
+int main(int, char**) {
+  Machine m;
+  sml::sm<Machine> sm(m);
+
+  while (true) {
+    std::string input;
+    std::cout << "Press [Enter] to continue, [q] to quit";
+    std::getline(std::cin, input);
+    if (input == "q") break;
+
+    sm.process_event(next{});
+  }
+  return 0;
+}
