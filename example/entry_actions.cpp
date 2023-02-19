78a4391737e30de502fee2707003c79cc2c171b5
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2023-02-19 11:25:16 +0100

    Calling a member function doesn't work

--- example/entry_actions.cpp
+++ example/entry_actions.cpp
@@ -7,37 +7,23 @@ namespace sml = boost::sml;
 struct next {};
 
 // states
-struct s1 {};
+struct s1 {
+  void on_entry() { std::cout << "member triggered\n"; }
+};
 struct s2 {};
 
-/* clang-format on */
-
-auto print = []() { std::cout << "lambda triggered\n"; };
-
-void free_print() { std::cout << "free_print triggered\n"; }
-
 class Machine {
-  using Self = Machine;
-  int data{0};
-
  public:
   auto operator()() {
     using namespace sml;
     /* clang-format off */
     return make_transition_table(
-      * "idle"_s      + on_entry<_> / print  // lambda expression works
-      // free functions and static members need to be wrapped
-      , state<s1>     + on_entry<_> / wrap(Self::static_print)
-      , state<s2>     + on_entry<_> / wrap(free_print)
-      , "idle"_s      + event<next> = state<s1>
+      * state<s1>     + on_entry<_> / &s1::on_entry
       , state<s1>     + event<next> = state<s2>
       , state<s2>     + event<next> = state<s1>
     );
     /* clang-format on */
   }
-
- private:
-  static void static_print() { std::cout << "static_print triggered\n"; };
 };
 
 int main(int, char**) {
