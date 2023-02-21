23df992531cd1eb6633337acf5596b7b1abdcaeb
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2023-02-21 04:02:52 +0100

    Working example
    Context is still copied a few times:
    ctor:   1
    copy c: 1 -> 2
    copy c: 2 -> 3
    copy c: 3 -> 4
    copy c: 2 -> 5
    method: 42, Hello (2)
    quitting

--- example/data.cpp
+++ example/data.cpp
@@ -1,79 +1,73 @@
-//
-// Copyright (c) 2016-2020 Kris Jusiak (kris at jusiak dot net)
-//
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-//
 #include <boost/sml.hpp>
-#include <boost/format.hpp>
-#include <cassert>
-#include <iostream>
+#include <cstdio>
+#include <queue>
+#include <string>
 
 namespace sml = boost::sml;
 
-namespace {
-struct connect {
-  int port{};
-};
-struct disconnect {};
-struct interrupt {};
+struct next {};
+struct quit {};
 
-struct Disconnected {};
-struct Connected {
-  int port{};  /// per state data
-};
-struct Interrupted {
-  int port{};  /// per state data
+const auto idle = sml::state<class idle>;
+const auto s1 = sml::state<class s1>;
+const auto s2 = sml::state<class s2>;
+
+struct Context {
   static int next_id;
-  int id{};
 
-  Interrupted(int port=-1) : port(port), id(next_id++) { p("ctor"); }
-  Interrupted(const Interrupted& other) : port(other.port), id(next_id++) { p((boost::format("copy ctor: %1% ->") % other.id).str().c_str(), false); };
-  Interrupted& operator=(const Interrupted& other) { port = other.port; p("copy assign"); return *this;};
-  Interrupted(Interrupted&& other) : port(other.port), id(next_id++) { p("move ctor"); };
-  Interrupted& operator=(Interrupted&& other) { port = other.port; p("move assign"); return *this;};
-  ~Interrupted() { p("dtor"); }
+  Context() = delete;
+  // user-defined constructors
+  Context(int data, const std::string& extra) : id(++next_id), data(data), extra(extra) { printf("ctor:   %d\n", id); }
 
-  void p(const char* op, bool colon=true) { printf(" %s%s %d\n", op, colon ? ":" : "", id); }
+  Context(const Context& c) : id(++next_id), data(c.data), extra(c.extra) { printf("copy c: %d -> %d\n", c.id, id); };
+  Context(Context&& c) : id(++next_id), data(c.data), extra(std::move(c.extra)) { printf("move c: %d -> %d\n", c.id, id); };
+
+  Context& operator=(const Context& c) = delete;
+  Context& operator=(Context&& c) = delete;
+
+  int id;
+  int data;
+  std::string extra;
 };
-int Interrupted::next_id = 0;
-
-class data {
-  using Self = data;
-
- public:
-  explicit data(const std::string& address) : address{address} {}
-
-  auto operator()() {
-    using namespace boost::sml;
-
-    const auto set = [](const auto& event, Connected& state) { state.port = event.port; };
-    const auto update = [](Connected& src_state, Interrupted& dst_state) { dst_state.port = src_state.port; };
-
-    // clang-format off
-    return make_transition_table(
-      * state<Disconnected> + event<connect>    / (set, &Self::print)    = state<Connected>
-      , state<Connected>    + event<interrupt>  / (update, &Self::print) = state<Interrupted>
-      , state<Interrupted>  + event<connect>    / (set, &Self::print)    = state<Connected>
-      , state<Connected>    + event<disconnect> / (&Self::print)         = X
-    );
-    // clang-format on
-  }
-
- private:
-  void print(Connected& state) { std::cout << address << ':' << state.port << '\n'; };
-
-  std::string address{};  /// shared data between states
-};
-}  // namespace
+int Context::next_id = 0;
 
 int main() {
-  data d{std::string{"127.0.0.1"}};
-  sml::sm<data> sm{d, Connected{1}, Interrupted()};
-  sm.process_event(connect{1024});
-  sm.process_event(interrupt{});
-  sm.process_event(connect{1025});
-  sm.process_event(disconnect{});
-  assert(sm.is(sml::X));
+  struct sub {
+    // sub must not have any user-defined constructor
+    Context ctx;  // state-local data
+
+    auto operator()() const noexcept {
+      using namespace sml;
+      // clang-format off
+      return make_transition_table(
+        * s1   + on_entry<_> / (&sub::method, process(quit{}), process(next{}))
+        , s2   + on_entry<_> / (process(quit{}), process(next{}))
+        , s1   + event<next> = s2
+        , s2   + event<next> = s1
+      );
+      // clang-format on
+    }
+    void method() { printf("method: %d, %s (%d)\n", ctx.data, ctx.extra.c_str(), ctx.id); }
+  };
+
+  struct parent {
+    sml::front::state_sm<sub>::type s;
+
+    auto operator()() noexcept {
+      using namespace sml;
+      // clang-format off
+      return make_transition_table(
+        * idle + event<next> = s
+        , s + event<quit> / [this] { printf("quitting\n"); } = X
+      );
+      // clang-format on
+    }
+  };
+
+  Context c(42, "Hello");  // initialize context with custom ctor
+  sub s{c};                // initialize sub-state by passing c via aggregate initialization
+
+  sml::sm<parent, sml::process_queue<std::queue>> sm(s);  // inject initialized sub state into state machine
+  sm.process_event(next{});
+  sm.process_event(next{});  // + process(e2{})
 }
